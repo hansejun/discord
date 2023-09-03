2baa69e8f953a41ee77338f52867e915e57b3f30
hansejun <86880916+hansejun@users.noreply.github.com>
2023-09-03 23:19:50 +0900

    Add: Upload File

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -23,6 +23,7 @@ import {
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { useEffect, useState } from 'react';
+import FileUpload from '@/components/common/file-upload';
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -67,7 +68,20 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO:Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FileUpload
+                        endpoint="serverImage"
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                      <FormControl>File Upload</FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
