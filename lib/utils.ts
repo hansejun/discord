12538bddec6f5245968f3c1ecc3510636cccd176
hansejun <86880916+hansejun@users.noreply.github.com>
2023-09-04 21:08:00 +0900

    Add: util fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { cloneDeep as clone } from 'lodash';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function lastItem<T>(arr: T[]) {
+  return arr[arr.length - 1];
+}
+
+export function lastIndex<T>(arr: T[]) {
+  return arr.length - 1;
+}
+
+export function toEqual<T>(item1: T, item2: T) {
+  return item1 === item2;
+}
+
+export function cloneDeep<T>(item: T) {
+  return cloneDeep(item);
 }
