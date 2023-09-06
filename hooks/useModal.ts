c8df26c52ec2b09e77c9c63d6a72721d4c689217
hansejun <86880916+hansejun@users.noreply.github.com>
2023-09-07 00:15:37 +0900

    Fix: useModal 수정

--- hooks/useModal.ts
+++ hooks/useModal.ts
@@ -1,27 +1,33 @@
+import { Server } from '@prisma/client';
 import { atom, useRecoilState } from 'recoil';
 
-export type ModalType = 'createServer';
+export type ModalType = 'createServer' | 'invite';
+
+interface ModalData {
+  server?: Server;
+}
 
 interface Modal {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
 }
 
 const modalAtom = atom<Modal>({
   key: 'modal',
-  default: { type: null, isOpen: false },
+  default: { type: null, isOpen: false, data: {} },
 });
 
 export const useModal = () => {
-  const [{ type, isOpen }, setValue] = useRecoilState(modalAtom);
+  const [{ type, isOpen, data }, setValue] = useRecoilState(modalAtom);
 
-  const onOpen = (type: ModalType) => {
-    setValue({ isOpen: true, type });
+  const onOpen = (type: ModalType, data = {}) => {
+    setValue({ isOpen: true, type, data });
   };
 
   const onClose = () => {
-    setValue({ isOpen: false, type: null });
+    setValue({ isOpen: false, type: null, data: {} });
   };
 
-  return { type, isOpen, onOpen, onClose };
+  return { type, isOpen, onOpen, onClose, data };
 };
