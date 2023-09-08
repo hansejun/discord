8eb046ea3470349b1bab8b0d43047621871cd29d
hansejun <86880916+hansejun@users.noreply.github.com>
2023-09-09 03:48:26 +0900

    Add: Manage Member

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import CreateServerModal from '../modals/create-server-modal';
 import InviteModal from '@/components/modals/invite-modal';
 import EditServerModal from '@/components/modals/edit-server-modal';
+import MembersModal from '@/components/modals/members-modal';
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
--- hooks/useModal.ts
+++ hooks/useModal.ts
@@ -1,7 +1,7 @@
 import { Server } from '@prisma/client';
 import { atom, useRecoilState } from 'recoil';
 
-export type ModalType = 'createServer' | 'invite' | 'editServer';
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members';
 
 interface ModalData {
   server?: Server;
