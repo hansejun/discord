6359efa086bd864a49a9321219a5e0c0d339804d
hansejun <86880916+hansejun@users.noreply.github.com>
2023-09-12 00:12:02 +0900

    Add: Channel Id Page & Redirect General Channel

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,45 @@
-const ServerIdPage = () => {
-  return <div>Server Id Page</div>;
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/modelUtils/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface PropsType {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: PropsType) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerIdPage;
