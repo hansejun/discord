9a1ad5bb30d31e324ff8f1a290aed6863a2bf8bf
hansejun <86880916+hansejun@users.noreply.github.com>
2023-09-17 02:21:03 +0900

    Fix: Params Error

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { SearchIcon } from 'lucide-react';
-import { useCallback, useEffect, useState } from 'react';
+import { SearchIcon } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -9,10 +9,10 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from '@/components/ui/command';
-import { useParams, useRouter } from 'next/navigation';
+} from "@/components/ui/command";
+import { useParams, useRouter } from "next/navigation";
 
-type Type = 'channel' | 'member';
+type Type = "channel" | "member";
 
 interface PropsType {
   data: {
@@ -34,38 +34,39 @@ const ServerSearch = ({ data }: PropsType) => {
   const [open, setOpen] = useState(false);
 
   const down = useCallback((e: KeyboardEvent) => {
-    if ((e.metaKey || e.ctrlKey) && e.key === 'k') {
+    if ((e.metaKey || e.ctrlKey) && e.key === "k") {
       e.preventDefault();
-      setOpen(prev => !prev);
+      setOpen((prev) => !prev);
     }
   }, []);
 
   const onClick = ({ id, type }: { id: string; type: Type }) => {
     setOpen(false);
 
-    if (type === 'member')
+    if (!params?.serverId) return;
+    if (type === "member")
       return router.push(`/server/${params.serverId}/conversations/${id}`);
 
-    if (type === 'channel')
+    if (type === "channel")
       return router.push(`/servers/${params.serverId}/channels/${id}`);
   };
 
   useEffect(() => {
-    document.addEventListener('keydown', down);
-    return () => document.removeEventListener('keydown', down);
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
   }, [down]);
 
   return (
     <>
       <button
         onClick={() => setOpen(true)}
-        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+        className="group flex w-full items-center gap-x-2 rounded-md px-2 py-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50"
       >
-        <SearchIcon className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
-        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+        <SearchIcon className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300">
           Search
         </p>
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+        <kbd className="pointer-events-none ml-auto inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
           <span>⌘</span>K
         </kbd>
       </button>
